837fdb949300270c23c1589e7027ebffcb1bc6e7
kieren roberts <kierenjroberts@gmail.com>
2022-07-17 19:21:38 +0100

    Exercise 1 in control flow complete

---
+++ ControlFlow/Exercise1ControlFlow/Program.cs
@@ -0,0 +1,13 @@
+﻿var number = 0;
+
+Console.WriteLine("Please enter a number between 1-10:\n ");
+number = Int32.Parse(Console.ReadLine());
+
+if (number >=1 && number <=10)
+{
+    Console.WriteLine("Valid");
+}
+else
+{
+    Console.WriteLine("Invalid");
+}
