4f9dbc419ef02673d7cb00ffe59d21c54429b476
kieren roberts <kierenjroberts@gmail.com>
2022-07-17 19:29:28 +0100

    Exercise 3 in Loops and Rand complete

---
+++ LoopsandRand/Exercise3LoopsandRand/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine("Please enter a number you would like to factorial: ");
+var number = Int32.Parse(Console.ReadLine());
+
+var total = number;
+for (var i = number-1; i > 1; i--)
+{
+    total *= i;
+}
+
+Console.WriteLine($"{total}");
