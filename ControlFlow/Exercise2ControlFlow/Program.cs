a1a39ee54e6f1f354c28624a9037fc9ef46a504c
kieren roberts <kierenjroberts@gmail.com>
2022-07-17 19:23:19 +0100

    Exercise 2 in Control Flow complete

---
+++ ControlFlow/Exercise2ControlFlow/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.Design;
+
+var number1 = 0;
+var number2 = 0;
+
+Console.WriteLine("Please enter your first number: \n");
+number1 = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Please enter your second number: \n");
+number2 = Int32.Parse(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.WriteLine("The highest number is: {0}", number1);
+}
+else
+{
+    Console.WriteLine("The highest number is: {0}", number2);
+}
