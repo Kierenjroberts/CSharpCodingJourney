0c6d182381568050fdf077442d6cd406d1fcf937
kieren roberts <kierenjroberts@gmail.com>
2022-07-17 19:29:40 +0100

    Exercise 4 in Loops and Rand complete

---
+++ LoopsandRand/Exercise4LoopsandRand/Program.cs
@@ -0,0 +1,31 @@
+﻿var rand = new Random();
+var number = rand.Next(1, 10);
+var lives = 4;
+
+while (lives > 0)
+{
+    Console.WriteLine("Please guess a number between 1 - 10: ");
+    var playerChoice = Int32.Parse(Console.ReadLine());
+        if (playerChoice == number)
+    {
+        Console.WriteLine("You have guessed correctly!");
+        Console.WriteLine($"You had {lives} lives remaining.");
+        break;
+        
+    }
+        else 
+        {
+            lives -= 1;
+            Console.WriteLine($"You have {lives} lives remaining.");
+        }
+}
+
+if (lives == 0)
+{
+    Console.WriteLine("You ran out of lives");
+}
+else
+{
+    Console.WriteLine("Congratulations");
+}
+
