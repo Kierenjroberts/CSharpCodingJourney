76e3d6d71d23609dcf9bbf5486c77fed3de698e4
kieren roberts <kierenjroberts@gmail.com>
2022-07-17 19:24:25 +0100

    Exercise 3 in Control Flow complete

---
+++ ControlFlow/Exercise3ControlFlow/Program.cs
@@ -0,0 +1,21 @@
+﻿
+var width = 0;
+var height = 0;
+
+Console.WriteLine("What is the width of your image? \n");
+width = Int32.Parse(Console.ReadLine());
+Console.WriteLine("What is the height of your image? \n");
+height = Int32.Parse(Console.ReadLine());
+
+if (height > width)
+{
+    Console.WriteLine("Your image is portrait.");
+}
+else if (width > height)
+{
+    Console.WriteLine("Your image is landscape.");
+}
+else
+{
+    Console.WriteLine("Your image is a Square.");
+}
