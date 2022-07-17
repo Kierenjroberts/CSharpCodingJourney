875705d5c14c57b0e7ba34b47feb83666a42adee
kieren roberts <kierenjroberts@gmail.com>
2022-07-17 19:29:17 +0100

    Exercise 2 in Loops and Rand complete

---
+++ LoopsandRand/Exercise2LoopsandRand/Program.cs
@@ -0,0 +1,13 @@
+﻿var number = 0;
+var total = 0;
+
+while(true)
+{
+    Console.WriteLine("Please enter a number or ok: ");
+    var response = Console.ReadLine();
+    if (response == "OK")
+        break;
+    number = Int32.Parse(response);
+    total += number;
+}
+Console.WriteLine($"The total is: {total}");
