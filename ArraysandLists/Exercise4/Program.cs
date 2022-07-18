33bf8e394a77f1340b1de9b14b65f2f559434d6e
Kierenjroberts <98908813+Kierenjroberts@users.noreply.github.com>
2022-07-18 19:43:08 +0100

    Arrays and Lists

---
+++ ArraysandLists/Exercise4/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Write a program and ask the user to continuously enter a number or type "Quit" to exit.
+ The list of numbers may include duplicates. 
+Display the unique numbers that the user has entered. */
+
+var numbers = new List <int>();
+
+while (true)
+{
+    Console.WriteLine("Please enter a number or type Quit: ");
+    var input = Console.ReadLine();
+
+    if (input.ToLower() == "quit")
+    {
+        break;
+    }
+
+    numbers.Add(Convert.ToInt32(input));
+}
+
+var uniqueEntries = new List<int>();
+foreach (var number in numbers)
+{
+    if (!uniqueEntries.Contains(number))
+    {
+        uniqueEntries.Add(number);
+    }
+}
+
+Console.WriteLine("Unique numbers:");
+foreach (var number in uniqueEntries)
+    Console.WriteLine(number);
