21c63c7cdedbca056aa38498e3a8902d18227cc9
Kierenjroberts <98908813+Kierenjroberts@users.noreply.github.com>
2022-07-18 15:27:02 +0100

    Update Program.cs

--- ArraysandLists/Exercise2/Program.cs
+++ ArraysandLists/Exercise2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Write a program and ask the user to enter their name.
+ Use an array to reverse the name and then store the result in a new string. 
+Display the reversed name on the console.
+*/
+
+var name = "";
+Console.WriteLine("Please enter a name: ");
+name = Console.ReadLine();
+
+char[] letters = name.ToCharArray();
+Array.Reverse(letters);
+
+Console.WriteLine(letters);
