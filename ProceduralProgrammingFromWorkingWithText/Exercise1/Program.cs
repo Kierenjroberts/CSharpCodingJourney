4be9eb79d240d4f3feb1315674d920c5608cb7c1
Kierenjroberts <98908813+Kierenjroberts@users.noreply.github.com>
2022-07-19 22:58:42 +0100

    Exercise 1 with methods

---
+++ ProceduralProgrammingFromWorkingWithText/Exercise1/Program.cs
@@ -0,0 +1,44 @@
+﻿/* Write a program and ask the user to enter a few numbers separated by a hyphen.
+ Work out if the numbers are consecutive.
+ For example, if the input is "5-6-7-8-9" or "20-19-18-17-16", display a message: "Consecutive"; otherwise, display "Not Consecutive". */
+
+
+Console.WriteLine("Please enter a set of numbers separated by a hyphen: ");
+
+
+var numberString = Console.ReadLine().Split('-');
+
+
+static bool IsNumbersConsecutive(String[] numberString)
+{
+    var numbersList = new List<int>();
+    var consecutive = true;
+
+    foreach (var number in numberString)
+    {
+        numbersList.Add(Convert.ToInt32(number));
+    }
+
+    numbersList.Sort();
+
+    for (var i = 1; i < numbersList.Count; i++)
+    {
+        if (numbersList[i] != numbersList[i - 1] + 1)
+        {
+            consecutive = false;
+            break;
+        }
+    }
+    return consecutive;
+}
+
+
+
+if (IsNumbersConsecutive(numberString) == true)
+{
+    Console.WriteLine("Is consecutive.");
+}
+else
+{
+    Console.WriteLine("Is not consecutive.");
+}
