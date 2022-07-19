430a115fe8fd8ee3c01eb96bce859716fdd5a44c
Kierenjroberts <98908813+Kierenjroberts@users.noreply.github.com>
2022-07-19 22:22:21 +0100

    Fixed naming convention

---
+++ WorkingWithText/Exercise2/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Write a program and ask the user to enter a few numbers separated by a hyphen.
+ If the user simply presses Enter, without supplying an input, exit immediately. 
+Otherwise, check to see if there are duplicates. If so, display "Duplicate" on the console. */
+
+Console.WriteLine("Please enter a set of numbers separated by a hyphen: ");
+
+var numberStringHyphen = Console.ReadLine();
+if (String.IsNullOrWhiteSpace(numberStringHyphen))
+{
+    return;
+}
+
+var numberString = numberStringHyphen.Split('-');
+var numbersList = new List<int>();
+
+foreach (var number in numberString)
+{
+    numbersList.Add(Convert.ToInt32(number));
+}
+
+numbersList.Sort();
+
+var duplicates = false;
+for (int i = 1; i < numbersList.Count; i++)
+{
+    if (numbersList[i] == numbersList[i - 1])
+    {
+        duplicates = true;
+        break;
+    }
+}
+
+var output = duplicates ? "Duplicates" : "No duplicates";
+Console.WriteLine(output);
