4d824f9d301fb3bd53c6e55f59a32ee5f00ce67b
Kierenjroberts <98908813+Kierenjroberts@users.noreply.github.com>
2022-07-21 20:02:49 +0100

    WorkingWithFiles update

--- WorkingWithFiles/Exercise1/Program.cs
+++ WorkingWithFiles/Exercise1/Program.cs
@@ -1,5 +1,7 @@
-﻿var path = @"C:\Users\k_j_r\Documents\GitHub\CodingJourney\WorkingWithFiles";
+﻿var path = @"E:\Documents\ExerciseText.txt";
 
 var textFromFile = File.ReadAllText(path);
-
 Console.WriteLine(textFromFile);
+
+var wordCount = textFromFile.Split(' ');
+Console.WriteLine(wordCount.Length);
---
+++ WorkingWithFiles/Exercise2/Program.cs
@@ -0,0 +1,24 @@
+﻿var path = @"E:\Documents\ExerciseText.txt";
+
+var textFromFile = File.ReadAllText(path);
+Console.WriteLine(textFromFile);
+
+var wordList = textFromFile.Split(' ');
+var wordCount = 0;
+
+foreach (var x in wordList)
+{
+    Console.WriteLine(x);
+}
+
+
+
+foreach (var word in wordList)
+{
+    if (wordCount < Convert.ToInt32(word.Length))
+    {
+        wordCount = word.Length;
+    }
+}
+
+Console.WriteLine(wordCount);
