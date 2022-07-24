413c4a8a1675da252d56ff5c5e1a8ce976ce67f2
Kierenjroberts <98908813+Kierenjroberts@users.noreply.github.com>
2022-07-24 20:32:32 +0100

    Exercise 1 code logic working
    Exercise 1 code logic working. Numbers are not returning correctly though. Suspect issues using DateTime.Now and subtracting.

--- Classes/Exercise1/Program.cs
+++ Classes/Exercise1/Program.cs
@@ -1,5 +1,26 @@
-﻿using Exercise1;
+﻿/* Design a class called Stopwatch. The job of this class is to simulate a stopwatch. It should
+   provide two methods: Start and Stop. We call the start method first, and the stop method next.
+   Then we ask the stopwatch about the duration between start and stop. Duration should be a
+   value in TimeSpan. Display the duration on the console.
+   We should also be able to use a stopwatch multiple times. So we may start and stop it and then
+   start and stop it again. Make sure the duration value each time is calculated properly.
+   We should not be able to start a stopwatch twice in a row (because that may overwrite the initial
+   start time). So the class should throw an InvalidOperationException if its started twice. */
 
-var cookie = new HttpCookie();
-cookie["name"] = "Mosh";
-Console.WriteLine(cookie["name"]);
+
+using System.Diagnostics;
+using Exercise1;
+
+var stopwatch = new StopWatch();
+var timeElapsed = new TimeSpan();
+
+Console.WriteLine("Press enter to start the stopwatch");
+stopwatch.initialiseWatch = Console.ReadLine();
+
+var startTime = stopwatch.Start();
+
+Console.WriteLine("Press enter to stop the stopwatch");
+stopwatch.endWatch = Console.ReadLine();
+timeElapsed = stopwatch.StopAndCalculateDuration(startTime);
+
+Console.WriteLine(timeElapsed);
--- Classes/Exercise1/StopWatch.cs
+++ Classes/Exercise1/StopWatch.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace Exercise1
 {
-    internal class StopWatch
+    public class StopWatch
     {
+        public string initialiseWatch;
+        public string endWatch;
+
+        public DateTime Start()
+        {
+            var startTime = new DateTime();
+
+            if (String.IsNullOrWhiteSpace(initialiseWatch))
+            {
+                startTime = DateTime.Now;
+            }
+
+            return startTime;
+        }
+
+        public TimeSpan StopAndCalculateDuration(DateTime startTime)
+        {
+            var endTime = new DateTime();
+            var initialTime = startTime;
+            var duration = new TimeSpan();
+
+            if (String.IsNullOrWhiteSpace(endWatch))
+            {
+                duration = endTime - initialTime;
+            }
+
+            return duration;
+        }
     }
 }
